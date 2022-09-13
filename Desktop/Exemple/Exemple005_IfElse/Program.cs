3a7fe3513ea138acffaf9c722fcca9426e054e93
Aleksandr Khatuntsev <hatuntsev.alexander@yandex.ru>
2022-09-13 21:20:05 +0300

    If Else

---
+++ Desktop/Exemple/Exemple005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "галя")
+{
+    Console.WriteLine("Ура, это же Галя!!!");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
